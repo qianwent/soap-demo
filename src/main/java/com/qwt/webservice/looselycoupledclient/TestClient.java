ea1b45aba25f6417ded3cfc1dceb72857a42c57b
Will Qian <qianwent@hotmail.com>
2018-01-30 11:58:47 -0500

    log4j needs both slf4j-api and slf4j-log4j12 in pom, and log4j.xml to initiaialize log4j system

--- src/main/java/com/qwt/webservice/looselycoupledclient/TestClient.java
+++ src/main/java/com/qwt/webservice/looselycoupledclient/TestClient.java
@@ -1,5 +1,8 @@
 package com.qwt.webservice.looselycoupledclient;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
 import java.net.MalformedURLException;
@@ -7,7 +10,11 @@ import java.net.URL;
 
 public class TestClient {
 
+	private static final Logger logger = LoggerFactory.getLogger(TestClient.class);
+
 	public static void main(String[] args) throws MalformedURLException {
+
+		logger.debug("entering into main method.");
 
 		URL url = new URL("http://localhost:8888/ns?wsdl");
 		QName qname = new QName("http://server.webservice.qwt.com/", "MyServiceImplService");
